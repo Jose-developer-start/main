c586105a4005b1d154414c83e9b66c9397c9aff5
Jose Deodanes <josedeodanes99@gmail.com>
2021-09-06 20:13:58 -0600

    V 1.2.9

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,4 +9,4 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
 
-mix.browserSync('http://127.0.0.1:8001/');
+mix.browserSync('http://127.0.0.1:8000/');
