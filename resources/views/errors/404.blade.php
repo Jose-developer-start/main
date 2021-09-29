322cd6af23cba0daa8f45500b951bb56b485833b
hilda <hilamaya10@gmail.com>
2021-09-28 21:38:38 -0600

    add error 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,18 @@
-<h1>No existe el contenido solicitado</h1>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<body>
+    <div class="page-wrap d-flex flex-row align-items-center">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 text-center">
+                <span class="display-1 d-block">404</span>
+                <div class="mb-4 lead">Solicitud no encontrada</div>
+                <a href="http://127.0.0.1:8000/" class="btn btn-link">Regresar</a>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
