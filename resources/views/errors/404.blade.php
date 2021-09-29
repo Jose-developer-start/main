423084409bdfad0a9bfd6359310906f57f067ff9
hilda <hilamaya10@gmail.com>
2021-09-28 21:57:51 -0600

    add error 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -10,7 +10,7 @@
             <div class="col-md-12 text-center">
                 <span class="display-1 d-block">404</span>
                 <div class="mb-4 lead">Solicitud no encontrada</div>
-                <a href="http://127.0.0.1:8000/" class="btn btn-link">Regresar</a>
+                <a href="{{url('/')}}"s class="btn btn-link">Regresar</a>
             </div>
         </div>
     </div>
